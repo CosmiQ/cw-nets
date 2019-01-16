5c2592f35b4a3b6a12fe9a323b369ff4d39361f2
Nick Weir <nrweir@users.noreply.github.com>
2019-01-16 14:34:37 -0500

    Revert "Iss17 organization of the repo (#18)" (#19)
    This reverts commit 6a4856c6e5ff111bf1a0515fb35f18f492432033.

--- cw_nets/base_net_class/base_net.py
+++ cw_nets/base_net_class/base_net.py
@@ -1,17 +1,25 @@
-# This class is a template for creating a transferable model
+## This class is a template for creating a transferable model
 import rasterio
 
 
-class BaseNet():
+class base_net():
+    
     def __init__(self, GSD_meters, image_size, bands=[]):
-        # Band Order assumed to be RGB if unspecified
+        #Band Order assumed to be RGB is unspecified
+        
         self.GSD_meters = gsd_meters
         self.image_size = image_size
+        
         if not bands:
-            self.bands = [1, 2, 3]
+            self.bands = [1,2,3]
         else:
             self.bands = bands
-
+            
+    
     def load_img(file_path):
+        
         with rasterio.open(file_path) as src:
-            # TODO: COMPLETE
+            
+
+            
+        
