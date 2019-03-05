35607bd1afcd8580bf83da987fa093ccc0fffcbd
nrweir <nicholas.r.weir@gmail.com>
2019-03-05 12:40:52 -0500

    fixing test import for cw_nets.models submodules

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -5,6 +5,6 @@ class TestImports(object):
         from cw_nets.data import datagen
         from cw_nets.data import io
         from cw_nets.data import transform
-        from cw_nets.models.configs import callbacks, infer, io, \
+        from cw_nets.models import callbacks, infer, io, \
             losses, metrics, train, zoo
         import cw_nets.utils as utils
